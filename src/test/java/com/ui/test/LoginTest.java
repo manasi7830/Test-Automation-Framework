15c7a1f8cc883b3ed9f1597f3d75e3b140087791
Manasi <Manasiavachat14@gmail.com>
2026-08-19 12:14:54 +0530

    Merge branch 'master' of https://github.com/manasi7830/Test-Automation-Framework.git

--- src/test/java/com/ui/test/LoginTest.java
+++ src/test/java/com/ui/test/LoginTest.java
@@ -26,23 +26,23 @@ public class LoginTest extends TestBase {
 
 	}
 
-//	@Test(description = "Verifies with the valid user is able to login into the application", groups = { "Sanity",
-//			"e2e" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVTestDataProvider")
-//	public void loginCSVTest(User user) {
-//
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
-//				"Manasi avachat");
-//
-//	}
-//
-//	@Test(description = "Verifies with the valid user is able to login into the application", groups = { "Sanity",
-//			"e2e" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listners.MyRetryAnalyzer.class)
-//
-//	public void loginExcelTest(User user) {
-//
-//		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
-//				"Manasi avachat1");
-//
-//	}
-//
+	@Test(description = "Verifies with the valid user is able to login into the application", groups = { "Sanity",
+			"e2e" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVTestDataProvider")
+	public void loginCSVTest(User user) {
+
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Manasi avachat");
+
+	}
+
+	@Test(description = "Verifies with the valid user is able to login into the application", groups = { "Sanity",
+			"e2e" }, dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestExcelDataProvider", retryAnalyzer = com.ui.listners.MyRetryAnalyzer.class)
+
+	public void loginExcelTest(User user) {
+
+		assertEquals(homePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName(),
+				"Manasi avachat1");
+
+	}
+
 }
